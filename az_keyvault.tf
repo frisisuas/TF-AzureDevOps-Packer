438362cf2bdfd704ae110440ee12686befd8001f
Raúl Carvajal B <44669926+frisisuas@users.noreply.github.com>
2021-06-07 16:03:55 +0200

    Update az_keyvault.tf

--- az_keyvault.tf
+++ az_keyvault.tf
@@ -38,37 +38,45 @@ resource "azurerm_key_vault_access_policy" "kv_pol_sp" {
   tenant_id    = data.azurerm_client_config.user_extract.tenant_id
   object_id    = azuread_service_principal.tf_sp.object_id
   key_permissions = [
-    "get", "list", "delete"
+    "get", "list", "update", "create", "import", "delete", "recover", "backup", "restore", "purge"
   ]
   secret_permissions = [
-    "get", "list", "delete"
+    "get", "list", "delete", "recover", "backup", "restore", "set", "purge"
   ]
   certificate_permissions = [
-    "get", "list"
+    "get", "list", "update", "create", "import", "delete", "recover", "backup", "restore", "deleteissuers", "getissuers", "listissuers", "managecontacts", "manageissuers", "setissuers"
   ]
 }
 
 # Creación de los secretos para los pipelines:
 resource "azurerm_key_vault_secret" "ADOAppID" {
-  depends_on   = [time_sleep.wait_30_seconds]
+  depends_on = [
+    azurerm_key_vault_access_policy.kv_pol_sp
+  ]
   key_vault_id = azurerm_key_vault.key_vault.id
   name         = "ADOAppID"
   value        = azuread_service_principal.tf_sp.application_id
 }
 resource "azurerm_key_vault_secret" "ADOAppSecret" {
-  depends_on   = [time_sleep.wait_30_seconds]
+    depends_on = [
+    azurerm_key_vault_access_policy.kv_pol_sp
+  ]
   key_vault_id = azurerm_key_vault.key_vault.id
   name         = "ADOAppSecret"
   value        = azuread_service_principal_password.tf_sp_pass.value
 }
 resource "azurerm_key_vault_secret" "StorageAccountName" {
-  depends_on   = [time_sleep.wait_30_seconds]
+    depends_on = [
+    azurerm_key_vault_access_policy.kv_pol_sp
+  ]
   key_vault_id = azurerm_key_vault.key_vault.id
   name         = "StorageAccountName"
   value        = azurerm_storage_account.stg_acc.name
 }
 resource "azurerm_key_vault_secret" "StorageAccountKey" {
-  depends_on   = [time_sleep.wait_30_seconds]
+    depends_on = [
+    azurerm_key_vault_access_policy.kv_pol_sp
+  ]
   key_vault_id = azurerm_key_vault.key_vault.id
   name         = "StorageAccountKey"
   value        = azurerm_storage_account.stg_acc.primary_access_key
