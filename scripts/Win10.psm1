02e928ce9f4d1fc9e496d68d2b1d25c8910e4928
Raúl Carvajal B <44669926+frisisuas@users.noreply.github.com>
2021-06-30 12:03:51 +0200

    Add files via upload

--- scripts/Win10.psm1
+++ scripts/Win10.psm1
@@ -4077,27 +4077,5 @@ Function UnpinTaskbarIcons {
 
 
 
-##########
-#region Auxiliary Functions
-##########
-
-# Wait for key press
-Function WaitForKey {
-	Write-Output "`nPress any key to continue..."
-	[Console]::ReadKey($true) | Out-Null
-}
-
-# Restart computer
-Function Restart {
-	Write-Output "Restarting..."
-	Restart-Computer
-}
-
-##########
-#endregion Auxiliary Functions
-##########
-
-
-
 # Export functions
 Export-ModuleMember -Function *
